62be52f28b89cbcf3e34226603da50b81a3213d7
Thanatpon Apaisuthepporn <33807432+ththapa@users.noreply.github.com>
2021-01-14 15:42:03 +0700

    Upload React Demo

---
+++ src/components/footer/footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="main-footer">
+        <strong>
+          Copyright © 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.
+        </strong>
+        All rights reserved.
+        <div className="float-right d-none d-sm-inline-block">
+          <b>Version</b> 3.1.0-rc
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
